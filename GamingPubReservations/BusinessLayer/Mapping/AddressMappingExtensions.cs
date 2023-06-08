6fc4cdfb9eb74f95aa63573d8dc7b5d2ad25d498
Bostan Adrian <bostan.adrian02@gmail.com>
2023-06-08 12:56:04 +0300

    Added MappingExtentions class for Address and GamingPub

---
+++ GamingPubReservations/BusinessLayer/Mapping/AddressMappingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Dtos;
+using DataAccessLayer.Entities;
+
+namespace BusinessLayer.Mapping
+{
+    public static class AddressMappingExtensions
+    {
+        public static Adress ToAddress(this AddAddressDto addressDto)
+        {
+            Adress address = new Adress
+            {
+                Country = addressDto.Country,
+                City = addressDto.City,
+                Street = addressDto.Street,
+                ZipCode = addressDto.ZipCode,
+                Number = addressDto.Number
+            };
+
+            return address;
+        }
+    }
+}
---
+++ GamingPubReservations/BusinessLayer/Mapping/GamingPubMappingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Dtos;
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Mapping
+{
+    public static class GamingPubMappingExtensions
+    {
+        public static GamingPub ToGamingPub(this AddGamingPubDto addGamingPubDto)
+        {
+            return new GamingPub
+            {
+                Name = addGamingPubDto.Name,
+                Adress = addGamingPubDto.AddAdressDto.ToAddress(),
+                PhoneNumber = addGamingPubDto.PhoneNumber
+            };
+        }
+    }
+}
